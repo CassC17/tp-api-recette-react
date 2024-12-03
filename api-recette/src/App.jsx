bd6a90c6f33623cac8f52123934b7dbf2e9f1290
CassC17 <cassandra.cinna@supdevinci-edu.fr>
2024-12-03 14:37:50 +0100

    add Footer

--- api-recette/src/App.jsx
+++ api-recette/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Header'
+import Footer from './Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,13 +16,8 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Footer />
     </>
   )
 }
