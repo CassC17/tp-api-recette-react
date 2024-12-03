3039dc92284fd095d08870c0d6e408066f5f5700
CassC17 <cassandra.cinna@supdevinci-edu.fr>
2024-12-03 14:33:49 +0100

    add header

---
+++ api-recette/src/Header.jsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  
+    return (
+      <>
+        <header>
+          <nav>
+            <ul>
+                <li> Acceuil </li>
+                <li> Favoris </li>
+            </ul>
+          </nav>
+        </header>
+      </>
+    )
+  }
+
+export default Header
