167a7512b6b7c9dbf95ee3aeb28c2215986a9b3b
CassC17 <cassandra.cinna@supdevinci-edu.fr>
2024-12-03 14:56:26 +0100

    fetch api cocktails

--- api-recette/src/CocktailList.jsx
+++ api-recette/src/CocktailList.jsx
@@ -1,7 +1,27 @@
 const CocktailList = () => {
+
+    const [cocktails, setCocoktails] = useState([])
+
+    // récup des cocktails
+    fetch('www.thecocktaildb.com/api/json/v1/1/search.php?s=a')
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        setCocoktails(data);
+    })
+
   return (
     <section>
-    <h1>Cocktail List</h1>
+        <h1>Cocktail List</h1>
+        {cocktails.map((cocktail) => (
+            <div key={cocktail.id}>
+                <h2>{cocktail.strDrink}</h2>
+                <p>{cocktail.strAlcoholic}</p>
+            </div>
+        ))}
     </section>
   )
 }
+
+export default CocktailList
