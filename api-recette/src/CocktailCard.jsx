c1fcbbf42c5ef2a1b5f61ef1038dc0a5dabd26b4
CassC17 <cassandra.cinna@supdevinci-edu.fr>
2024-12-04 11:46:13 +0100

    install react router dom

--- api-recette/src/CocktailCard.jsx
+++ api-recette/src/CocktailCard.jsx
@@ -1,18 +1,19 @@
 const CocktailCard = ({ cocktail }) => {
     return (
-        <article>
+        <article key={cocktail.idDrink}>
+    
             <p>{cocktail.strDrink}</p>
             <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink}/>
             <p>{cocktail.strInstructions}</p>
-                  <ul>
-                      {Object.keys(cocktail)
-                          .filter(key => key.startsWith('strIngredient') && cocktail[key])
-                          .map((key, index) => (
-                              <li key={index}>
-                                  {cocktail[key]} - {cocktail['strMeasure${index + 1}']}
-                              </li>
+                <ul>
+                    {Object.keys(cocktail)
+                        .filter(key => key.startsWith('strIngredient') && cocktail[key])
+                        .map((key, index) => (
+                            <li key={index}>
+                                {cocktail[key]} - {cocktail['strMeasure${index + 1}']}
+                            </li>
                           ))}
-                  </ul>
+                </ul>
         </article>
     );
 }
