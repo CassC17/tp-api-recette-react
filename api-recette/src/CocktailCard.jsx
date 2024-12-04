6a1c0d74efeca6616b7bf7db7be77c1cc0acddaa
CassC17 <cassandra.cinna@supdevinci-edu.fr>
2024-12-04 11:49:01 +0100

    Revert "install react router dom"
    This reverts commit c1fcbbf42c5ef2a1b5f61ef1038dc0a5dabd26b4.

--- api-recette/src/CocktailCard.jsx
+++ api-recette/src/CocktailCard.jsx
@@ -1,19 +1,18 @@
 const CocktailCard = ({ cocktail }) => {
     return (
-        <article key={cocktail.idDrink}>
-    
+        <article>
             <p>{cocktail.strDrink}</p>
             <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink}/>
             <p>{cocktail.strInstructions}</p>
-                <ul>
-                    {Object.keys(cocktail)
-                        .filter(key => key.startsWith('strIngredient') && cocktail[key])
-                        .map((key, index) => (
-                            <li key={index}>
-                                {cocktail[key]} - {cocktail['strMeasure${index + 1}']}
-                            </li>
+                  <ul>
+                      {Object.keys(cocktail)
+                          .filter(key => key.startsWith('strIngredient') && cocktail[key])
+                          .map((key, index) => (
+                              <li key={index}>
+                                  {cocktail[key]} - {cocktail['strMeasure${index + 1}']}
+                              </li>
                           ))}
-                </ul>
+                  </ul>
         </article>
     );
 }
